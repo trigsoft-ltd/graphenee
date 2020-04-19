6b4332fc000377d5c27330e35673ee7d68d58f86
Farrukh Ijaz <farrukh.ijaz@trigsoft.com>
2020-04-20 05:34:17 +0900

    change in mvn-deploy.sh script

--- graphenee/mvn-deploy.sh
+++ graphenee/mvn-deploy.sh
@@ -1,4 +1,1 @@
-mvn -f core/pom.xml package deploy -DskipTests
-mvn -f vaadin7/pom.xml package deploy -DskipTests
-mvn -f jbpm/pom.xml package deploy -DskipTests
-mvn -f blockchain/pom.xml package deploy -DskipTests
+mvn -f ./pom.xml package deploy -DskipTests
